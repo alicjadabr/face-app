da1f328c9fa5f40da149139ad38e5e6b5cc06ec2
Alicja Dabrowska <alicja.dab27@gmail.com>
2022-09-26 20:07:35 +0200

    features update

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,8 +1,12 @@
+import { type } from '@testing-library/user-event/dist/type';
 import React, { useState } from 'react';
 import "./imageLinkForm.css"
 
 const ImageLinkForm = () => {
   const [inputValue, setInputValue] = useState('');
+  const [boxValues, setBoxValues] = useState({});
+  const [fame, setFame] = useState('Unknown');
+  const [chance, setChance] = useState('?');
 
   const handleInputChange = e => {
     setInputValue(e.target.value)
@@ -25,7 +29,31 @@ const ImageLinkForm = () => {
       }
     ]
   });
-  
+
+  const calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const celebrity = data.outputs[0].data.regions[0].data.concepts[0].name;
+    const chances = data.outputs[0].data.regions[0].data.concepts[0].value;
+    setFame(celebrity);
+    setChance(chances.toString().slice(2,4));
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  const getFaceBox = (box) => {
+    setBoxValues(boxValues => ({
+      ...boxValues,
+      ...box
+    }));
+  }
+
   const requestOptions = {
     method: 'POST',
     headers: {
@@ -36,8 +64,8 @@ const ImageLinkForm = () => {
   };
 
   fetch("https://api.clarifai.com/v2/models/celebrity-face-detection/versions/2ba4d0b0e53043f38dbbed49e03917b6/outputs", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
+  .then(response => response.json())
+  .then(result => getFaceBox(calculateFaceLocation(result)))
   .catch(error => console.log('error', error));
 
   }
@@ -56,7 +84,11 @@ const ImageLinkForm = () => {
     </div>
     <div className='center'>
       <div className="absolute mt4">
-        <img alt="" src={inputValue} width='650px' heigh='auto' />
+        <img id='inputImage' alt="" src={inputValue} width='650px' heigh='auto' />
+        <div className='bounding-box' style={{top: boxValues.topRow, bottom: boxValues.bottomRow, left: boxValues.leftCol, right: boxValues.rightCol}}></div>
+      <div>
+        <h2 className='ba b--gold pa3 br3 grow'>Result: {fame} for {chance}%</h2>
+      </div>
       </div>
     </div></>
   );
